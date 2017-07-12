c03b9e869c3ba5f5146e8a28f49a7176be3e0cdf
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-07-12 20:38:55 +0100

    Handles case where user is not in a node package

--- index.js
+++ index.js
@@ -36,36 +36,51 @@ cachelessRequire.resolve = require.resolve;
 function main() {
 
   const packagepath = findUp.sync('package.json');
-  const rootpath = path.dirname(packagepath);
-  const pckg = require(packagepath);
 
-  console.log(``);
-  console.log(` \u2022 Node Testing Repl for package: "${chalk.cyan(pckg.name)}".`);
-  console.log(` \u2022 require() will search for modules relative to:`);
-  console.log(`     ${chalk.green(rootpath)}`);
-  console.log(` \u2022 Caching of modules loaded using require is disabled.`);
-  console.log(``);
+  let rootpath, packagename;
+
+  if (packagepath == null) {
+
+    console.log(``);
+    console.log(`   ** WARNING **`);
+    console.log(` \u2022 The package root could not be found.`);
+    console.log(` \u2022 If you are attempting to test a node package make sure that there is a`);
+    console.log(`   package.json file in your project.`);
+    console.log(` \u2022 Try running ${chalk.magenta('npm init')} in the root directory.`);
+
+    rootpath = path.resolve('./');
+    packagename = 'unknown-package';
+  } else {
+    rootpath = path.dirname(packagepath);
+    packagename = require(packagepath).name;
+  }
 
   if (path.parse(path.resolve(rootpath, '..')).base === 'node_modules') {
-    
+
     // get package user probably wants.
     let likelyRootPath = rootpath;
-    
+
     do {
       likelyRootPath = path.dirname(findUp.sync('package.json', {
         cwd: path.resolve(likelyRootPath, '..'),
       }));
     } while(path.parse(path.resolve(likelyRootPath, '..')).base === 'node_modules')
-    
+
     const likely_package = require(path.join(likelyRootPath, 'package.json'));
-    
-    console.warn(`   ** WARNING **`);
-    console.warn(` \u2022 This repl seems to have been invoked from within node_modules.`);
-    console.warn(` \u2022 If you intended to test the package: "${chalk.cyan(likely_package.name)}" please navigate`);
-    console.warn(`   out of the node_modules directory.`);
+
     console.log(``);
+    console.log(`   ** WARNING **`);
+    console.log(` \u2022 This REPL seems to have been invoked from within node_modules.`);
+    console.log(` \u2022 If you intended to test the package: "${chalk.cyan(likely_package.name)}" please navigate`);
+    console.log(`   out of the node_modules directory.`);
   }
-    
+
+  console.log(``);
+  console.log(` \u2022 Node testing REPL for package: "${chalk.cyan(packagename)}".`);
+  console.log(` \u2022 require() will search for modules relative to:`);
+  console.log(`     ${chalk.green(rootpath)}`);
+  console.log(` \u2022 Caching of modules loaded using require is disabled.`);
+  console.log(``);
 
   let testingRepl = repl.start();
 
