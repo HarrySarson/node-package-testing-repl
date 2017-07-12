7b79bc466256447ec721032d22af440b50d15cc5
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-07-12 19:59:50 +0100

    refactors script into a main function

--- index.js
+++ index.js
@@ -11,13 +11,8 @@ const os            = require('os')
     , replHistory   = require('repl.history')
     ;
 
-const packagepath = findUp.sync('package.json');
-const rootpath = path.dirname(packagepath);
-const pckg = require(packagepath);
-
 const replHistoryFile = path.join(os.homedir(), '.node-test-repl-history');
   
-
 function cachelessRequire(filePath = '') {
   let modulePath;
   
@@ -38,40 +33,48 @@ function cachelessRequire(filePath = '') {
 
 cachelessRequire.resolve = require.resolve;
 
-console.log(``);
-console.log(` \u2022 Node Testing Repl for package: "${chalk.cyan(pckg.name)}".`);
-console.log(` \u2022 require() will search for modules relative to:`);
-console.log(`     ${chalk.green(rootpath)}`);
-console.log(` \u2022 Caching of modules loaded using require is disabled.`);
-console.log(``);
+function main() {
 
-if (path.parse(path.resolve(rootpath, '..')).base === 'node_modules') {
-  
-  // get package user probably wants.
-  let likelyRootPath = rootpath;
-  
-  do {
-    likelyRootPath = path.dirname(findUp.sync('package.json', {
-      cwd: path.resolve(likelyRootPath, '..'),
-    }));
-  } while(path.parse(path.resolve(likelyRootPath, '..')).base === 'node_modules')
-  
-  const likely_package = require(path.join(likelyRootPath, 'package.json'));
-  
-  console.warn(`   ** WARNING **`);
-  console.warn(` \u2022 This repl seems to have been invoked from within node_modules.`);
-  console.warn(` \u2022 If you intended to test the package: "${chalk.cyan(likely_package.name)}" please navigate`);
-  console.warn(`   out of the node_modules directory.`);
+  const packagepath = findUp.sync('package.json');
+  const rootpath = path.dirname(packagepath);
+  const pckg = require(packagepath);
+
   console.log(``);
+  console.log(` \u2022 Node Testing Repl for package: "${chalk.cyan(pckg.name)}".`);
+  console.log(` \u2022 require() will search for modules relative to:`);
+  console.log(`     ${chalk.green(rootpath)}`);
+  console.log(` \u2022 Caching of modules loaded using require is disabled.`);
+  console.log(``);
+
+  if (path.parse(path.resolve(rootpath, '..')).base === 'node_modules') {
+    
+    // get package user probably wants.
+    let likelyRootPath = rootpath;
+    
+    do {
+      likelyRootPath = path.dirname(findUp.sync('package.json', {
+        cwd: path.resolve(likelyRootPath, '..'),
+      }));
+    } while(path.parse(path.resolve(likelyRootPath, '..')).base === 'node_modules')
+    
+    const likely_package = require(path.join(likelyRootPath, 'package.json'));
+    
+    console.warn(`   ** WARNING **`);
+    console.warn(` \u2022 This repl seems to have been invoked from within node_modules.`);
+    console.warn(` \u2022 If you intended to test the package: "${chalk.cyan(likely_package.name)}" please navigate`);
+    console.warn(`   out of the node_modules directory.`);
+    console.log(``);
+  }
+    
+
+  let testingRepl = repl.start();
+
+  replHistory(testingRepl, replHistoryFile);
+
+  testingRepl.context.require = cachelessRequire;
 }
-  
 
-let testingRepl = repl.start();
-
-replHistory(testingRepl, replHistoryFile);
-
-testingRepl.context.require = cachelessRequire;
-
+main();
 
 
 
